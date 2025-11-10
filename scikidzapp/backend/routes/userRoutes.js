eb1dbe1d16e9395d9d4d1a2d201445cff4332af3
kevin <kevintate2@my.unt.edu>
2025-11-10 14:46:20 -0600

    Troubleshooting adjustments for user route

--- scikidzapp/backend/routes/userRoutes.js
+++ scikidzapp/backend/routes/userRoutes.js
@@ -5,12 +5,19 @@ const router = express.Router()
 
 router.post('/', async (req, res) => {
     try {
+        console.log('Incoming body: ', req.body)
         const { name, email, password } = req.body;
+
+if (!name || !email || !password) {
+    return res.status(400).json({ error: 'All fields required'})
+}
+
         const user = new User({
             name,
             email,
             passwordHash: password
         });
+        
         await user.save()
         res.status(201).json(user)
     } catch (err) {
