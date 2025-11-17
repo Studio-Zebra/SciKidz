c605ccc8bfb9edcbb1c23f95cf8e91402a2d5b0a
kevin <kevintate2@my.unt.edu>
2025-11-17 16:55:19 -0600

    adding additional dependencies + security
    additional dependencies + json web token support

--- scikidzapp/backend/server.js
+++ scikidzapp/backend/server.js
@@ -7,6 +7,7 @@ import { connectDB } from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import progressRoutes from './routes/progressRoutes.js'
 import lessonRoutes from './routes/lessonRoutes.js'
+import authRoutes from './routes/authRoutes.js'
 
 dotenv.config();
 connectDB();
@@ -18,6 +19,7 @@ app.use(express.json());
 //test
 app.get('/', (req, res) => res.send('Backend API running!'));
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/progress', progressRoutes);
 app.use('/api/lessons', lessonRoutes);
