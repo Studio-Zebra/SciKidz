4d7d71eb66c7767ae27a2ddc88167c0241c8237e
kevin <kevintate2@my.unt.edu>
2025-11-17 15:20:16 -0600

    added update and delete operations for rest api

--- scikidzapp/backend/routes/userRoutes.js
+++ scikidzapp/backend/routes/userRoutes.js
@@ -30,4 +30,28 @@ router.get('/', async (req, res) => {
     res.json(users)
 })
 
+router.put('/:id', async (req, res) => {
+    const updated = await User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+if (!updated) return res.status(404).json({ error: 'User not found' });
+res.json(updated);
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+        if (!deletedUser) {
+            return res.status(404).json ({ error: 'User not found' });
+        }
+
+        res.json({ message: 'User deleted' });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
 export default router
