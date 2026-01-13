edcc3b7531ae5277ae50b50141a3c2b828a7aa67
kevin <kevintate2@my.unt.edu>
2026-01-12 18:45:24 -0600

    attempting to use standard connection string to allow testing in any location

--- scikidzapp/backend/server.js
+++ scikidzapp/backend/server.js
@@ -1,3 +1,6 @@
+import dns from "dns"
+dns.setDefaultResultOrder("ipv4first")
+
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -11,6 +14,7 @@ import authRoutes from './routes/authRoutes.js'
 
 dotenv.config();
 connectDB();
+
 
 const app = express();
 app.use(cors());
