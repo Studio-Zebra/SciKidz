f834f27e161c792677c5a28a9a34624c332a5487
soriphoono <soriphoono@gmail.com>
2026-04-22 17:20:25 -0500

    Updated flake with ansible bootstrap script

--- flake.nix
+++ flake.nix
@@ -18,14 +18,9 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    github-actions-nix = {
-      url = "github:synapdeck/github-actions-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs @ {
-    self,
     nixpkgs,
     flake-parts,
     ...
@@ -37,7 +32,6 @@
         agenix-shell.flakeModules.default
         treefmt-nix.flakeModule
         git-hooks-nix.flakeModule
-        github-actions-nix.flakeModule
       ];
       systems = import inputs.systems;
       agenix-shell.secrets = (import ./secrets.nix {inherit lib;}).agenix-shell-secrets;
@@ -55,82 +49,13 @@
         devShells.default = import ./shell.nix {
           inherit lib pkgs;
           config = {
-            inherit (config) pre-commit agenix-shell githubActions;
+            inherit (config) pre-commit agenix-shell;
           };
         };
 
         # --- Configuration Builders --- #
-        githubActions = import ./actions.nix {inherit self lib;};
         treefmt = import ./treefmt.nix {inherit lib pkgs;};
         pre-commit = import ./pre-commit.nix {inherit lib pkgs;};
-
-        apps = rec {
-          deploy = {
-            type = "app";
-            program = "${
-              pkgs.writeShellApplication {
-                name = "deploy";
-                runtimeInputs = with pkgs; [
-                  docker
-                ];
-                text = ''
-                  nix run .#teardown
-
-                  sleep 10 # Wait for all services to disapear
-
-                  # Create required networks
-                  if ! docker network ls | grep -q proxy; then
-                    docker network create --driver overlay --attachable proxy
-                  fi
-
-                  # Deploy the new stack
-                  docker stack deploy -c docker/swarm-cd/docker-compose.yml swarm-cd
-                '';
-              }
-            }/bin/deploy";
-          };
-
-          teardown = {
-            type = "app";
-            program = "${
-              pkgs.writeShellApplication {
-                name = "teardown";
-                runtimeInputs = with pkgs; [
-                  docker
-                ];
-                text = ''
-                  STACKS="${lib.concatStringsSep
-                    " "
-                    (
-                      builtins.attrNames
-                      (builtins.readDir ./docker)
-                    )}"
-
-                  # Remove each stack
-                  echo "Removing stacks: $STACKS"
-                  for stack in $STACKS; do
-                    echo "Removing stack: $stack"
-                    docker stack rm "$stack"
-                  done
-
-                  sleep 10
-
-                  if docker network ls | grep -q proxy; then
-                    echo "Removing network: proxy"
-                    docker network rm proxy
-                  fi
-
-                  if [ "$#" -eq 1 ] && [ "$1" == "--complete" ] && docker volume ls | grep -q scikidz_mongodb-data; then
-                    echo "Removing volume: scikidz_mongodb-data"
-                    docker volume rm scikidz_mongodb-data
-                  fi
-                '';
-              }
-            }/bin/teardown";
-          };
-
-          default = deploy;
-        };
       };
     };
 }
--- shell.nix
+++ shell.nix
@@ -13,24 +13,17 @@ with pkgs;
       age
       agenix-cli
       sops
+
+      (python3.withPackages (ps:
+        with ps; [
+          ansible-core
+          ansible
+        ]))
+      ansible-lint
     ];
 
     shellHook = ''
       ${config.pre-commit.shellHook}
       source ${config.agenix-shell.installationScript}/bin/install-agenix-shell
-
-      # Deploy GitHub Actions from actions.nix when that file is modified to create reactive checks in GitHub CI
-      mkdir -p .github/workflows
-      ${lib.concatStringsSep "\n" (
-        lib.mapAttrsToList (
-          name: file: let
-            safeName = lib.removeSuffix ".yml" name;
-          in ''
-            cp -f ${file} ./.github/workflows/${safeName}.yml
-            chmod +w ./.github/workflows/${safeName}.yml
-          ''
-        )
-        config.githubActions.workflowFiles
-      )}
     '';
   }
